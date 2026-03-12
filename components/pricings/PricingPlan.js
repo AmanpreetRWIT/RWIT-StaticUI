16e37c4a0aa9f5fc2cf9cc892b71987d9c944703
Gaurav Shukla <shukla.gaurav@rwit.io>
2026-03-12 12:26:17 +0530

    Update pricing section

--- components/pricings/PricingPlan.js
+++ components/pricings/PricingPlan.js
@@ -1,97 +1,84 @@
-import React from "react";
-import SectionTitle from "../common/SectionTitle";
-import PricingIcons, { InfoIcons } from "./PricingIcons";
-import Button from "../buttons/Button";
+import React from 'react';
+import SectionTitle from '../common/SectionTitle';
+import PricingIcons from './PricingIcons';
+import { InfoIcons } from './PricingIcons';
+import Button from '../buttons/Button';
 
 const PricingPlan = ({ data }) => {
   return (
     <div
-      id="PricingPlane"
-      className={`pricing d-flex align-items-center`}
+      id='PricingPlane'
+      className={`pricing d-flex align-items-center  `}
       style={
-        data?.BGColor?.color
+        data?.BGColor
           ? {
-              backgroundColor: `${data?.BGColor?.color}`,
+              backgroundColor: `${data?.BGColor}`,
             }
           : {}
       }
     >
       <div className={`pricing-container container`}>
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="axil-call-to-action position-relative">
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='axil-call-to-action position-relative'>
               <SectionTitle
-                titleClass=""
+                titleClass=''
                 title={data?.Heading}
                 subtitle={data?.Tags}
-                alignment="center"
+                alignment='center'
                 description={data?.Description}
-                titleColor={data?.HeadingColor?.color}
-                descriptionColor={
-                  data?.DescriptionColor?.color
-                    ? data?.DescriptionColor?.color
-                    : ""
-                }
+                titleColor={data?.HeadingColor}
+                descriptionColor={data?.DescriptionColor}
               />
 
-              <div className="text-center"></div>
+              <div className='text-center'></div>
             </div>
 
-            <div className="compare"></div>
+            <div className='compare'></div>
           </div>
         </div>
-        <div className="pricing-wrapper">
+        <div className='pricing-wrapper '>
           {data?.PricingCard?.map((Item, index) => {
             return (
               <div
-                className={`pricing-table ${
-                  Item?.Featured ? "featured-table" : ""
-                }`}
+                className={`pricing-table ${Item?.Featured ? 'featured-table' : ''}`}
                 key={index}
               >
                 {Item?.Featured && Item?.FeaturedText && (
-                  <div className="pricing-badge">
+                  <div className='pricing-badge'>
                     <p>{Item?.FeaturedText}</p>
                   </div>
                 )}
-                <div className="pricing-innertable">
+                <div className='pricing-innertable'>
                   {Item?.Title && (
-                    <div className="pricing-table-title">
-                      <h3 style={Item?.Featured ? { color: "#fff" } : {}}>
-                        {Item?.Title}
-                      </h3>
+                    <div className='pricing-table-title'>
+                      <h3 style={Item?.Featured ? { color: '#fff' } : {}}>{Item?.Title}</h3>
                     </div>
                   )}
                   {Item?.SubTitle && (
-                    <div className="pricing-table-subtitle">
-                      <p style={Item?.Featured ? { color: "#fff" } : {}}>
-                        {Item?.SubTitle}
-                      </p>
+                    <div className='pricing-table-subtitle'>
+                      <p style={Item?.Featured ? { color: '#fff' } : {}}>{Item?.SubTitle}</p>
                     </div>
                   )}
 
                   {Item?.Features?.map((feature, index) => {
                     return (
-                      <div className="pricing-table-feature" key={index}>
+                      <div className='pricing-table-feature' key={index}>
                         <span>
                           <PricingIcons isFeatured={Item?.Featured} />
                         </span>
-                        <div className="position-relative">
+                        <div className='position-relative'>
                           <p
-                            className={`${
-                              feature?.IsHover ? "featured-wrapper" : ""
-                            }`}
-                            style={Item?.Featured ? { color: "#fff" } : {}}
+                            className={`${feature?.IsHover ? 'featured-wrapper' : ''}`}
+                            style={Item?.Featured ? { color: '#fff' } : {}}
                           >
                             {feature?.Text}
 
                             {feature?.InfoText && feature?.IsHover && (
-                              <span className="featured-icon">
+                              <span className='featured-icon'>
                                 {feature?.InfoText && (
                                   <span
-                                    className={`${
-                                      feature?.InfoText ? "featured-text" : ""
-                                    }`}
+                                    className={`${feature?.InfoText ? 'featured-text' : ''} `}
                                   >
                                     {feature?.InfoText}
                                   </span>
@@ -107,26 +94,12 @@ const PricingPlan = ({ data }) => {
 
                   <div
                     className={`${
-                      Item?.Featured ? "featured-btn" : ""
-                    } pricing-btn d-flex pricing-btn`}
+                      Item?.Featured ? 'featured-btn' : ''
+                    } pricing-btn d-flex pricing-btn `}
                   >
-                    {Item?.Button?.length > 0 &&
-                      Item.Button.map((button, index) => {
-                        const link = button?.Link || {};
-                        const hrefCandidate =
-                          link?.story?.url ??
-                          link?.cached_url ??
-                          link?.url ??
-                          "";
-                        if (!hrefCandidate) return null;
-                        return (
-                          <Button
-                            key={button?._uid || `button-${index}`}
-                            blok={button}
-                            Index={index}
-                          />
-                        );
-                      })}
+                    {Item?.Button?.map((button, index) => (
+                      <Button key={button?._uid || `button-${index}`} blok={button} Index={index} />
+                    ))}
                   </div>
                 </div>
               </div>
